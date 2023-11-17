3715002238ff335c907bf9fa53dbc4cd3d1edc46
username <useremail>
2023-11-16 16:35:48 -0800

    Add project files.

---
+++ CIS110_Course_Project_1.py
@@ -0,0 +1,4 @@
+print(f"Hello, Would you like to hear a story about a farmdog?")
+print(f"Before we get started, I have a few questions for you.")
+print(f"After typing your answer, please press the enter key.")
+input(f"/nPress the enter  key to continue...")
